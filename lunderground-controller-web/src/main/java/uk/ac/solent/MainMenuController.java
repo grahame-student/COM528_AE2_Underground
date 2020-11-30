56cdbb1e82487ad28408647e35575795389d4bdf
Grahame White <4whitg16@solent.ac.uk>
2020-11-30 14:58:07 +0000

    Addressed more lint comments

--- lunderground-controller-web/src/main/java/uk/ac/solent/MainMenuController.java
+++ lunderground-controller-web/src/main/java/uk/ac/solent/MainMenuController.java
@@ -6,16 +6,26 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
+/**
+ * This is the controller for the main landing page http://localhost/lunderground
+ * This controller takes care of serving up the pages related to the various menu items
+ */
 public final class MainMenuController
 {
     @RequestMapping(value = "/", method = RequestMethod.GET)
-    public final String mainPage()
+    /**
+     * Link the root URL for the project to the main landing page
+     */
+    public String mainPage()
     {
         return "index";
     }
 
     @RequestMapping(value = "/manage-stations", method = RequestMethod.GET)
-    public final String manageStationsPage()
+    /**
+     * Link to the page responsible for managing underground stations
+     */
+    public String manageStationsPage()
     {
         return "manage-stations";
     }
--- lunderground-controller-web/src/main/java/uk/ac/solent/package-info.java
+++ lunderground-controller-web/src/main/java/uk/ac/solent/package-info.java
@@ -1,2 +1,4 @@
+/**
+ * This package is for the web app used to manage the shared underground resources
+ */
 package uk.ac.solent;
-
