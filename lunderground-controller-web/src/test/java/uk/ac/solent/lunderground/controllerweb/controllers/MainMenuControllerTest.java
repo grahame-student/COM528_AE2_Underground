a7ae04ae8fd7f1385d7a87472947c6649b754890
Grahame White <4whitg16@solent.ac.uk>
2020-12-12 11:57:55 +0000

    Addressed lint comments

--- lunderground-controller-web/src/test/java/uk/ac/solent/lunderground/controllerweb/controllers/MainMenuControllerTest.java
+++ lunderground-controller-web/src/test/java/uk/ac/solent/lunderground/controllerweb/controllers/MainMenuControllerTest.java
@@ -25,13 +25,22 @@ import static org.springframework.test.web.servlet.setup.MockMvcBuilders.webAppC
 @RunWith(SpringJUnit4ClassRunner.class)
 public class MainMenuControllerTest
 {
+    /**
+     * Web Application Context used to allow the test cases to run.
+     */
     @Autowired
     private WebApplicationContext context;
 
+    /**
+     * MockMvc instance used to request endpoints and verify that the correct outcomes occur.
+     */
     private MockMvc mockMvc;
 
+    /**
+     * Code to be executed before each test case to ensure that each one starts froms the same configuration.
+     */
     @Before
-    public void Setup()
+    public void setup()
     {
         mockMvc = webAppContextSetup(this.context).build();
     }
